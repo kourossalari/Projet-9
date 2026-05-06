8aeb29050056d5a245ccc3b6fc8bb4bbc14b74b6
saalarikouros@gmail.com <saalarikouros@gmail.com>
2026-05-06 16:56:38 +0200

    Séparation des deux map dans le composant Slides et attribution d'une clé correct

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -22,7 +22,6 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
@@ -38,20 +37,20 @@ const Slider = () => {
               </div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
+      ))}
+      
+      <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${radioIdx.id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
           </div>
-        </>
-      ))}
     </div>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -117,13 +117,13 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
+        { last && <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
           label={last?.type}
-        />
+        /> }
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
